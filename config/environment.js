03ccb28fa0e336844d931925f198400b736f522b
Tracy Lee <tracy@wingman.events>
2015-06-27 14:12:23 -0700

    ember-cli-github-pages

--- config/environment.js
+++ config/environment.js
@@ -5,7 +5,7 @@ module.exports = function(environment) {
     modulePrefix: 'venture-hacked-site',
     environment: environment,
     baseURL: '/',
-    locationType: 'auto',
+
     EmberENV: {
       FEATURES: {
         // Here you can enable experimental features on an ember canary build
@@ -43,7 +43,7 @@ module.exports = function(environment) {
   }
 
   if (environment === 'production') {
-
+    ENV.locationType = 'hash';
   }
 
   return ENV;
