c5c188d6a549958603a4b9b89df64a9bfd7c848d
Tracy Lee <tracy@wingman.events>
2015-08-08 13:30:10 -0700

    change-fingerprint

--- Brocfile.js
+++ Brocfile.js
@@ -2,7 +2,11 @@
 
 var EmberApp = require('ember-cli/lib/broccoli/ember-app');
 
-var app = new EmberApp();
+var app = new EmberApp({
+	fingerprint: {
+		exclude: 'img/album-7-22'
+	}
+});
 
 // Use `app.import` to add additional libraries to the generated
 // output files.
