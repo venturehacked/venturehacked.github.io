fcd06386f82521e5f11fae187f187f66592a8b1a
Tracy Lee <tracy@wingman.events>
2015-09-07 17:32:12 -0700

    add-investors-event

--- app/utils/investors.js
+++ app/utils/investors.js
@@ -1,73 +1,101 @@
 export default [{
+	cardTitle: 'Tom Kalinske',
+	title: 'Investor',
+	image: 'img/investors/tom-kalinske.jpg',
+	cardContent: '',
+	revealContent: "Tom Kalinske is the Chairman of Global Education Learning and former CEO of LeapFrog, Knowledge Universe, Sega, Matchbox, and Mattel. He has worked with brands such as Barbie, Hot Wheels, See N' Say, Masters of the Universe, Matchbox, Sega Genesis, Sonic the Hedgehog, LeapFrog, and more. Tom is also on the board of Cambium Learning (NASDAQ: ABCD) a company that has curriculum for children with special learning needs and which also helps turn-around troubled schools and is currently interested in using on-line technology, combined with great education pedagogy, creating fun experiences so children are engaged and thus learn enjoyably and faster.",
+	current: true
+}, {
+	cardTitle: 'Sanjit Dang',
+	title: 'Investor',
+	image: 'img/investors/sanjit-dang.jpg',
+	cardContent: 'Intel Capital',
+	revealContent: "Sanjit is an Investment Director at Intel Capital. He drives investments in Client Computing across Consumer and Enterprise sectors. He has also driven several investments in Big Data, IoT and Cloud Computing. Sanjit has over a decade of Exec leadership experience in Product Design, Business Development and Strategy across several domains: Big Data, Natural Language Processing, 3D Camera/Apps, Supply-Chain Analytics and Flash Memory. He has managed Billion dollar accounts and executed >30 partnership deals in $100M-$2Bill range. Always ahead of the curve, Sanjit worked on Big Data Analytics before industry created the term 'Big Data'. Similarly, he launched 2 online courses during grad school in 1999 and published iconic papers on learnings, way before the MOOC revolution started.  He’s currently an investor in DocuSign (eSignature), Reflektion (eCommerce), MongoDB (NoSQL Database), Enlighted (IoT), Mirantis (Cloud Computing), GoodData (SaaS BI) and Arcadia Data (Data Analytics 2.0), to name a few. He has had 2 Exits within 1 year of investment - Basis Science (Wearables, acq Intel) and Maginatics (Cloud Storage, acq EMC).",
+	current: true
+}, {
+	cardTitle: 'Ekta Dang',
+	title: 'Investor',
+	image: 'img/investors/placeholder.jpg',
+	cardContent: 'Intel Capital',
+	revealContent: "Ekta Dang works at Intel Capital on Enterprise/Consumer SW investments. She is actively looking for startups in the IOT/Cybersecurity/Big Data space. Ekta has 10 years of Senior Leadership experience at Intel with broad range of expertise including Corp Strategy, Business Development, IOT Product Management, ISV Management, IP Deal-making, Marketing, and Supply-Chain and has led several Partnerships/Alliances. She has executed >30 deals ranging from $5M to $1Billion. Ekta also has a strong technical background and a PhD in Physics.",
+	current: true
+}, {
+	cardTitle: 'Jeremy Edberg',
+	title: 'Angel Investor',
+	image: 'img/investors/jeremy-edberg-2.jpg',
+	cardContent: '',
+	revealContent: "Jeremy is an angel investor and advisor for various incubators and startups, and is the cofounder of CloudNative.  He was the founding Reliability Engineer for Netflix, the largest video streaming service in the world and he ran ops for reddit, an online community for sharing and discussing interesting things on the internet that did more a billion pageviews a month at the time.",
+	current: true
+}, {
 	cardTitle: 'Jennifer Carolan',
 	title: 'Investor',
 	image: 'img/investors/jennifer-carolan.jpg',
 	cardContent: 'Reach Capital',
 	revealContent: "Jennifer began her career in teaching and soon became interested in differentiated learning and technology tools. After studying at the Stanford School of Education, where she also taught, Jennifer joined NewSchools in 2006 and co-founded the Seed Fund in 2011. She has led over 40 investments and sits on the boards of six edtech companies. Jennifer developed a math camp for her kids and their friends, scuba dives with her daughter, and learns piano with her sons.",
-	current: true
+	current: false
 },{
 	cardTitle: 'Laurence "Lo" Toney',
 	title: 'Partner, Catalyst Fund',
 	image: 'img/investors/lo-toney.jpeg',
 	cardContent: 'Comcast Ventures',
 	revealContent: "Lo joined Comcast Ventures in 2014, and is responsible for the Comcast Ventures Catalyst Fund, which is focused on investing in early stage technology startups led by minority entrepreneurs. His investment focus is on consumer technologies including vertical ecommerce, marketplaces and on demand services.",
-	current: true
+	current: false
 },{
 	cardTitle: 'Andrea Zurek',
 	title: 'Founding Partner',
 	image: 'img/investors/andrea-zurek.jpg',
 	cardContent: 'XG Ventures',
 	revealContent: "Andrea Zurek, founding partner of XG Ventures, has been named one of the top 25 tech angels by Businessweek.  She has also been named one of 25 Influential Business Women in SF/Silicon Valley and profiled in the Top 40 Under 40 in the Silicon Valley Business Journal.  She was recently honored at the Women 2.0 \"Innovation Unleashed\" as one of five \"Catalyst\" Honorees. This recognition goes to five early stage investors who are creating impact in technology through their leadership and investment priorities. Since 2008, XG Ventures has invested in over 80 companies with 17 exits, including such companies as Facebook, Box, Disney Interactive and Twitter.  Andrea worked at Google for over 7 years as one of the first ad sales executives.  She has her BA/MBA from Santa Clara University.  She is a Board member of JUMA, the ASTIA Women Entrepreneurs Network and sits on the Board of Regents for Santa Clara University School of Business.  She has a passion for automobiles, kite surfing and running half marathons.",
-	current: true
+	current: false
 }, {
 	cardTitle: 'Bucky Moore',
 	title: 'Investor',
 	image: 'img/investors/bucky-moore.jpg',
 	cardContent: 'Costanoa Venture Capital',
 	revealContent: "Bucky joins Costanoa after 4+ years investing and working in and around technology companies. Prior to joining Costanoa, Bucky was an investor at Battery Ventures, where he focused on early stage investments in enterprise software and infrastructure. During his time at Battery, he invested in companies such as Veradocs and Lightcyber. Earlier in his career, Bucky was a member of the corporate development team at Cisco, where he led M&A and venture investment activity within the data center and cloud infrastructure segments. During his time at Cisco, he led investments in companies including Platfora, Embrane, 6Wind, Nantero, Whiptail, and Insieme Networks.",
-	current: true
+	current: false
 },{
 	cardTitle: 'Manny Fernandez',
 	title: 'Investor',
 	image: 'img/investors/manny-fernandez.jpg',
 	cardContent: 'DreamFunded',
 	revealContent: "Manny Fernandez is an angel investor and serial entrepreneur. Manny is San Francisco’s 2014 Angel Investor of the Year. He is the founder SF Angels Group and co-founder/ CEO of DreamFunded.com, an equity crowdfunding platform with over 4,000 global accredited investors. Follow this thought leader on Twitter @MannyFernandez.",
-	current: true
+	current: false
 }, {
 	cardTitle: 'Murray Newlands',
 	title: 'Investor',
 	image: 'img/investors/murray-newlands.jpg',
 	cardContent: 'Network of Things Fund',
 	revealContent: "Murray Newlands FRSA is an entrepreneur, business advisor and speaker. He is founder of Due an online invoicing company helping 75k+ business based in San Francisco. Newlands is also an adviser to the Draper Nexus Network of Things Fund that invests in IOT companies. He gives practice advice from the 1000’s of pitches he has heard for entrepreneurs and startups to grow their companies, get investment and succeed. Newlands received a Bachelor of Laws and he is qualified as a Lawyer. He gained his Green Card by being recognized by the US government as an “alien of extraordinary ability.” Newlands is the author of “Online Marketing: A User’s Manual” published by John Wiley & Sons. He is a contributor to Forbes and Entrepreneur.com.",
-	current: true
+	current: false
 }, {
 	cardTitle: 'Shuly Galili',
 	title: 'Co-Founder & Investor',
 	image: 'img/investors/shuly-ghalili.jpeg',
 	cardContent: 'UpWest Labs',
 	revealContent: "Shuly Galili is an investor and the Co-founder of UpWest Labs a Silicon Valley based fund investing in seed stage Israeli entrepreneurs looking grow their companies in the US. UpWest Labs (invested in over 50 companies) currently has the largest community of entrepreneurs innovating between Tel Aviv and San Francisco in domains such as Enterprise, Saas, Cyber Security, Commerce, Analytics and Data. Shuly spent the past decade building Silicon Valley’s gateway to Israel as the co-founder and Executive Director of The California Israel Chamber of Commerce.",
-	current: true
+	current: false
 }, {
 	cardTitle: 'Ho Nam',
 	title: 'General Partner',
 	image: 'img/investors/ho-nam.jpg',
 	cardContent: 'Altos Ventures',
 	revealContent: "Ho Nam is a General Partner of Altos Ventures, focusing on investments in the areas of software, mobile, and internet technologies. He currently serves as a director of DigitalPath, HireMojo, Lohika, Trilibis Mobile, TVU Networks, and WhiteHat Security. Ho led the firm’s prior investments in Axis Systems (Cadence), Enwisen (Infor), Evolve (Oracle), Immunet (Sourcefire), Listen.com (Real Networks), Nishan Systems (Brocade), Pixo (Sun) and SayNow (Google). Ho began his VC career at Trinity Ventures and began his professional career at Bain & Company where he advised clients in technology and consumer products industries. Before co-founding Altos Ventures, Ho worked at Silicon Graphics and Octel Communications.",
-	current: true
+	current: false
 },{
 	cardTitle: 'Bijan Yazdani',
 	title: 'Angel Investor',
 	image: 'img/investors/bijan-yazdani.jpg',
 	cardContent: '',
 	revealContent: "Bijan Yazdani is an active angel investor in the Silicon Valley with deep technical expertise.",
-	current: true
+	current: false
 }, {
 	cardTitle: 'John Rampton',
 	title: 'Angel Investor & Entrepreneur',
 	image: 'img/investors/john-rampton.png',
 	cardContent: '',
 	revealContent: "Best known as an Entrepreneur and Connector. John was recently named #3 on Top 50 Online Influencers in the World by Entrepreneur Magazine as well as a blogging expert by Forbes. Awarded Top 10 Most Influential PPC Experts in the World for the past 3 years. He currently advises several companies in the bay area.",
-	current: true
+	current: false
 }, {
 	cardTitle: 'Sara Thomas',
 	title: 'Investor',
@@ -81,7 +109,7 @@ export default [{
 	image: 'img/investors/salil-pradhan.jpg',
 	cardContent: 'Draper Nexus Ventures',
 	revealContent: "Salil is a Venture Partner and early stage investor with Draper Nexus Ventures. An advisor to corporate LPs on product strategy, partnerships with startups, and M&A, his current focus in on seed/early stage startups in verticals including retail, healthcare and agriculture with an emphasis on IoT (Internet of things).",
-	current: true
+	current: false
 }, {
 	cardTitle: 'Will Bunker',
 	title: 'Partner',
@@ -110,7 +138,7 @@ export default [{
 	image: 'img/investors/asif-giga.jpg',
 	cardContent: 'Singtel Innov8',
 	revealContent: "Asif is currently supporting the fund's North American investment activities. Prior to joining Singtel Innov8, Asif started his own startup while at Wharton receiving funding from the Wharton Innovation Fund. Before Wharton he spent time at Data Collective Venture Capital which focuses on early stage deals around Big Data & Analytics. Asif spent time with Accenture in their Management Consulting practice building his operating experience with clients such as eBay, Microsoft and Cisco. He also spent time in Ernst & Young's Advisory Service practice. Asif received a BS in Business Administration from the University of Southern California, MMS from Duke University and his MBA from the Wharton School, University of Pennsylvania where he was a recipient of The Meckler Family Fellowship.",
-	current: true
+	current: false
 }, {
 	cardTitle: 'Rob Majteles',
 	title: 'Founder',
@@ -131,7 +159,7 @@ export default [{
 	image: 'img/investors/jules-walker.jpg',
 	cardContent: 'KPMG Venture Capital Practice',
 	revealContent: 'Jules Walker leads the business development efforts for KPMG’s Emerging Technology practice in the Bay area and beyond, focusing on creating and expanding relationships with hot start-ups and pre-IPO companies. He has spent his 20+ year professional career in financial services working for companies such as Bank of America, Robert Half and Textron Financial.',
-	current: true
+	current: false
 }, {
 	cardTitle: 'Tyler Peterson',
 	title: 'Vice President',
@@ -153,14 +181,14 @@ export default [{
 	cardContent: 'DeNa',
 	revealContent: 'Miki is in charge of startup investment in US market at DeNA. Miki focuses on investments in consumer tech including mobile game tech, ecommerce, market place and on-line community. After having been at the forefront of the Internet industry in Japan last one and half decades, he relocated himself to San Francisco 2.5 years ago.'+
 	'Prior to DeNA, Miki has held multiple leadership positions including a board of director at Digital Garage, CEO at Open Network Lab, CTO/COO at Kakaku.com and practice manager at Netscape.',
-	current: true
+	current: false
 }, {
 	cardTitle: 'Eric Nagel',
 	title: 'Angel Investor',
 	image: 'img/investors/eric-nagel.jpg',
 	cardContent: '',
 	revealContent: 'Eric has over 37 years of experience in engineering, marketing and sales in the semiconductor industry. For the last 10 years, prior to his recent retirement, he was a consultant as a technology analyst to some of the biggest names on Wall Street . Eric has invested in, as well as worked at multiple startups in the semiconductor, solar, tech IP, wine and food industries.',
-	current: true
+	current: false
 }, {
 	cardTitle: 'Lin Tam',
 	title: 'Co-Founder',
@@ -188,7 +216,7 @@ export default [{
 	image: 'img/investors/steve-austin.jpg',
 	cardContent: '',
 	revealContent: "20 years experience in Silicon Valley high-tech from from start-ups to large companies. Focus on driving revenue & competitive advantages in biz dev opportunities – domestic and international – for mobile applications, energy and technology. Steve is an advisor to several start-ups, instructor at Founders Space, and is a frequent guest speaker on Go-To-Market strategies at local startup events.",
-	current: true
+	current: false
 },{
 	cardTitle: 'Jim Jensen',
 	title: 'Partner',
@@ -209,28 +237,28 @@ export default [{
 	image: 'img/investors/george-arabian.jpg',
 	cardContent: 'Steelhead VC',
 	revealContent: "We do seed-stage investments and work with founders, joint venture partners, thought leaders and executive teams who are making a positive difference in their communities & the future of our world. Our portfolio includes 25 visionary companies whose ideas will impact their marketplace and communities. We are activist investors who catalyze growth through access to our networks, added value through our expertise, & a focus on revenue acceleration.",
-	current: true
+	current: false
 }, {
 	cardTitle: 'Adam Boyden',
 	title: 'Investor',
 	image: 'img/investors/adam-boyden.png',
 	cardContent: 'RPM Ventures',
 	revealContent: "Adam Boyden is a five-time entrepreneur who has provided leadership for venture capital backed companies that have disrupted multi-billion dollar industries and generated more than $2 billion in exit value.",
-	current: true
+	current: false
 }, {
 	cardTitle: 'John Lim',
 	title: 'Analyst',
 	image: 'img/investors/john-lim.jpg',
 	cardContent: 'Fenox Venture Capital',
 	revealContent: "John leads Fenox Venture Capital's due diligence operations. Fenox is a Silicon Valley VC with a global footprint. With 8 funds raised and more than 50 investments made in 4 years, Fenox has grown at a breakneck pace. Prior to joining Fenox, John has experience in leading financial analysis and fundraising at a startup, as well as startup business development initiatives at a consulate general's trade and investment division.",
-	current: true
+	current: false
 },{
 	cardTitle: 'Kristen Bergman',
 	title: 'Mentor',
 	image: 'img/investors/kristen-bergman.jpg',
 	cardContent: '',
 	revealContent: "Kristen Bergman is Sr. Account Manager for Traction Interactive managing a team of 22 at Apple, and she is a Founding Member and former Vice President-Community Development of StartUp Cup. She has traveled extensively throughout the world developing entrepreneurial ecosystems by recruiting and training entrepreneurs and mentors, working with incubators and accelerators, developing experiential curriculum, and advising and educating angel investors / venture capitalists. Kristen specializes in pitch coaching and business model development.",
-	current: true
+	current: false
 }];
 
 
