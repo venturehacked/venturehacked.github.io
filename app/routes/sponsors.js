73cffe5097c34707e12ce2efc44c5eaa67a0256b
Tracy Lee <tracy@wingman.events>
2015-09-25 18:17:14 -0700

    edit cardtitle

--- app/routes/sponsors.js
+++ app/routes/sponsors.js
@@ -3,7 +3,7 @@ import Ember from 'ember';
 export default Ember.Route.extend({
 	model() {
 		return [{
-		scardTitle: "Wingman",
+		cardTitle: "Wingman",
 		sponsorImage: "img/wingman-logo.jpg",
 		sponsorUrl: "http://wingman.events",
 		sponsorContent: "Add Sponsor Content Here"
