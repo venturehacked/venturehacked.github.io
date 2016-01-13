52af18d9aa85981dcac3d6534e7120e9c2634b79
Jeremy W. Rowe <jeremywrowe@users.noreply.github.com>
2016-01-12 21:23:35 -0500

    moar logging

--- lib/try-generate-pages.sh
+++ lib/try-generate-pages.sh
@@ -1,6 +1,10 @@
 #!/bin/bash
+
+echo "Attempting to generate pages"
+echo "Pull request? ${TRAVIS_PULL_REQUEST}"
+echo "Branch ${TRAVIS_BRANCH}"
+
 if [[ "${TRAVIS_PULL_REQUEST}" == "false" && "${TRAVIS_BRANCH}" == "main" ]]; then
-  echo "Attempting to generate pages"
   ember github-pages:commit --message "DEPLOYED ON $(date +%Y-%m-%d) AT $(date +%H:%M:%S)" --branch "${GH_DEST_BRANCH}"
   echo "Generated a deployment"
   git push deploy master 2>&1 >/dev/null
