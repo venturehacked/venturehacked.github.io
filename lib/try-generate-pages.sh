cd44b092a17b73e43a1295152cd1751e422b28b9
Jeremy W. Rowe <jeremywrowe@users.noreply.github.com>
2016-01-12 21:20:38 -0500

    adds some debug

--- lib/try-generate-pages.sh
+++ lib/try-generate-pages.sh
@@ -1,8 +1,10 @@
 #!/bin/bash
-echo "Attempting to generate pages"
 if [[ "${TRAVIS_PULL_REQUEST}" == "false" && "${TRAVIS_BRANCH}" == "main" ]]; then
+  echo "Attempting to generate pages"
   ember github-pages:commit --message "DEPLOYED ON $(date +%Y-%m-%d) AT $(date +%H:%M:%S)" --branch "${GH_DEST_BRANCH}"
+  echo "Generated a deployment"
   git push deploy master 2>&1 >/dev/null
+  echo "Pushed to master"
 else
   echo "Not a main build -- no deploy"
 fi
