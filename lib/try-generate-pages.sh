f94ef19f80f22a9eab72aee20de41551484f6196
Jeremy W. Rowe <jeremywrowe@users.noreply.github.com>
2016-01-12 20:51:14 -0500

    fixes to generate pages

--- lib/try-generate-pages.sh
+++ lib/try-generate-pages.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 echo "Attempting to generate pages"
-if [[ "${TRAVIS_PULL_REQUEST}" = "false" && "${TRAVIS_BRANCH}" == 'main' ]]; then
-  ember github-pages:commit --message "New release" --branch "${GH_DEST_BRANCH}"
+if [[ "${TRAVIS_PULL_REQUEST}" == "false" && "${TRAVIS_BRANCH}" == "main" ]]; then
+  ember github-pages:commit --message "DEPLOYED ON $(date +%Y-%m-%d) AT $(date +%H:%M:%S)" --branch "${GH_DEST_BRANCH}"
   git push deploy master 2>&1 >/dev/null
 else
   echo "Not a main build -- no deploy"
