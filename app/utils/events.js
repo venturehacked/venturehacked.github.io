e06c0d59c2b96e1550e2f9f548147f8a43009829
Tracy Lee <tracy@wingman.events>
2016-05-13 17:14:07 -0400

    update events

--- app/utils/events.js
+++ app/utils/events.js
@@ -9,17 +9,18 @@ export default [
 //   eventContent: "",
 //   eventbriteLink: "//eventbrite.com/tickets-external?eid=20656478082&ref=etckt"
 // },
+// {
+//   date: "May 11, 7-930pm",
+//   image:"img/event-image-rdu.jpg",
+//   id: "2016-05-11-rdu",
+//   title: "Speed Investing & Lessons Learned",
+//   location: "Raleigh",
+//   address: "HQ Raleigh, 310 S Harrington St, Raleigh, NC",
+//   eventContent: "",
+//   eventbriteLink: "//eventbrite.com/tickets-external?eid=20657927417&ref=etckt",
+//   eventbritePage: "https://www.eventbrite.com/e/raleigh-launch-venture-hacked-speed-investing-mentoring-startups-entrepreneurs-tickets-20657927417"
+// },
 {
-  date: "May 11, 7-930pm",
-  image:"img/event-image-rdu.jpg",
-  id: "2016-05-11-rdu",
-  title: "Speed Investing & Lessons Learned",
-  location: "Raleigh",
-  address: "HQ Raleigh, 310 S Harrington St, Raleigh, NC",
-  eventContent: "",
-  eventbriteLink: "//eventbrite.com/tickets-external?eid=20657927417&ref=etckt",
-  eventbritePage: "https://www.eventbrite.com/e/raleigh-launch-venture-hacked-speed-investing-mentoring-startups-entrepreneurs-tickets-20657927417"
-},{
   date: "May 26, 7-930pm",
   image:"img/event-image-sv-2.jpg",
   id: "2016-05-26-sv",
