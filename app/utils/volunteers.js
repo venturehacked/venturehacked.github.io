eb0d4d7429bc51cca289841a41b7bff6308ccbc9
Tracy Lee <tracy@wingman.events>
2016-07-29 14:08:04 -0700

    add volunteer page and sponsor

--- app/utils/volunteers.js
+++
@@ -1,9 +0,0 @@
-export default [{
-	cardTitle: 'Neda Blocho',
-	title: 'Partner',
-	image: 'img/investors/neda-blocho.jpg',
-	cardContent: 'Pejman Mar Ventures',
-	revealContent: "",
-	current: true,
-	events: ['2016-08-18-sv']
-}];
